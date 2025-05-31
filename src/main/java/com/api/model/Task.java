2fe4a16c6c87bffae3914f931d542d71d331a91f
Nitin <nitin127001@gmail.com>
2025-05-31 07:08:25 +0000

    Add validation annotations to Task entity fields for name and description

--- src/main/java/com/api/model/Task.java
+++ src/main/java/com/api/model/Task.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Task {
@@ -11,8 +13,14 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 100, message = "Name must be at most 100 characters")
     private String name;
+
+    @Size(max = 255, message = "Description must be at most 255 characters")
     private String description;
+
     private boolean completed;
 
     // Constructors
